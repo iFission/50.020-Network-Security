03c0fcc8ce417ae5e9ce44f8e535591fa86e16b9
iFission <alexwwj@icloud.com>
2021-03-30 07:05:50 -0700

    feat: complete task4

--- lab6/tun_server.py
+++ lab6/tun_server.py
@@ -1,5 +1,27 @@
 #!/usr/bin/python3
+import fcntl
+import struct
+import os
+import time
 from scapy.all import *
+
+TUNSETIFF = 0x400454ca
+IFF_TUN = 0x0001
+IFF_TAP = 0x0002
+IFF_NO_PI = 0x1000
+
+# Create the tun interface
+tun = os.open("/dev/net/tun", os.O_RDWR)
+ifr = struct.pack('16sH', b'wang%d', IFF_TUN | IFF_NO_PI)
+ifname_bytes = fcntl.ioctl(tun, TUNSETIFF, ifr)
+
+# Get the interface name
+ifname = ifname_bytes.decode('UTF-8')[:16].strip("\x00")
+print("Interface Name: {}".format(ifname))
+
+# assign ip and bring up interface
+os.system("ip addr add 192.168.53.100/24 dev {}".format(ifname))
+os.system("ip link set dev {} up".format(ifname))
 
 IP_A = "0.0.0.0"
 PORT = 9090
@@ -12,3 +34,6 @@ while True:
     print("{}:{} --> {}:{}".format(ip, port, IP_A, PORT))
     pkt = IP(data)
     print(" Inside: {} --> {}".format(pkt.src, pkt.dst))
+
+    # write to tun
+    os.write(tun, bytes(pkt))
