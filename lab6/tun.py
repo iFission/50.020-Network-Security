9faf4ab3bb5d2072da0675123dba4dc323a9e4ad
iFission <alexwwj@icloud.com>
2021-03-30 05:24:08 -0700

    feat: complete task2d-2

--- lab6/tun.py
+++ lab6/tun.py
@@ -32,10 +32,5 @@ while True:
 
         # Send out a spoof packet using the tun interface
         if ip['ICMP'].type == 8:
-            newip = IP(src=ip.dst, dst=ip.src)
-            newpkt = newip / ICMP(
-                type=0, code=0, seq=ip['ICMP'].seq,
-                id=ip['ICMP'].id) / ip['ICMP'].load
-            print(newpkt.summary())
 
-            os.write(tun, bytes(newpkt))
+            os.write(tun, bytes(123))
